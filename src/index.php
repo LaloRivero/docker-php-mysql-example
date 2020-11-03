b2e9814b8ebf918f8c0532937c5f639e129ec6f6
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-11-03 11:46:45 -0700

    heroku.yml

--- src/index.php
+++ src/index.php
@@ -1,6 +1,39 @@
 
 <?php
 
-echo "Hello World from Docker";
+echo "Transactions with mysql";
+echo "<br>";
+
 
 $mysqli = new mysqli("db", "root", "helloworld", "db_test");
+
+if(mysqli_connect_errno()){
+    echo "ERROR";
+}
+
+$mysqli -> set_charset("utf8");
+$mysqli -> autocommit(FALSE);
+
+/* Start a transaction */
+$mysqli -> begin_transaction(MYSQLI_TRANS_START_WITH_CONSISTENT_SNAPSHOT);
+
+$sql = "INSERT INTO p_stock (product, p_qty) VALUES('ram',10), ('cpu',23);";
+
+if(($result=$mysqli -> query($sql))==FALSE){
+    $mysqli -> rollback();
+    echo "Some error happends";
+}else{
+    echo "Query ok";
+    echo "<br>";
+}
+
+$sql = "INSERT INTO p_stock (product, p_qty) VALUES('FAN',10), ('Power Supply',23);";
+
+if(($result=$mysqli -> query($sql))==FALSE){
+    $mysqli -> rollback();
+    echo "Some error happends";
+}else{
+    $mysqli -> commit();
+    echo "Query ok";
+    echo "<br>";
+}
